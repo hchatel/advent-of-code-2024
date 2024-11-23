ba7ba8a98802bfda10a83c501cef54bb48d3efa7
Hugo <chatelhugo@hotmail.fr>
2024-11-23 18:03:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Advent of Code
+
+Template to solve Advent of code easily with typescript and TDD (if you wish).
+
+# Init project
+
+[Install Deno](https://docs.deno.com/runtime/)
+
+Check you have deno installed
+
+```bash
+$ deno -v
+```
+
+You're set !
+
+# For each day
+
+- Copy template for that day
+
+```bash
+$ deno task new Day01
+```
+
+- Copy your test inputs into generated texts files
+
+- Run the solution
+
+```bash
+$ deno run --allow-read Day01/solution.ts
+```
+
+# Other commands
+
+## Run tests
+
+```bash
+$ deno test --allow-read Day01
+```
---
+++ template/solution.ts
@@ -0,0 +1,40 @@
+// --------------
+// Problem Inputs
+// --------------
+
+import { solveWithLogs } from "../utils/logs.ts";
+
+type InputLine = string;
+
+// Switch inputs here
+const USE_TEST_INPUT = true;
+
+// Read file
+const inputFilename = USE_TEST_INPUT ? "inputTest.txt" : "input.txt";
+const inputLines: InputLine[] = Deno.readTextFileSync(
+    `${import.meta.dirname}/${inputFilename}`,
+).split("\n");
+
+// -------------------
+// Your functions here
+// -------------------
+
+// -------------
+// Solve problem
+// -------------
+
+export const problem1 = () => {
+    console.log("Input lines: ", inputLines);
+
+    return "This problem ain't gonna solve itself !";
+};
+
+// const problem2: Solver = () => myFunction(TEST_INPUT);
+
+// ---------------
+// Display answers
+// ---------------
+
+solveWithLogs(problem1, 1);
+
+// solveWithLogs(problem2, 2);
